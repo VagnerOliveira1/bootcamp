4731177578cdc10ede57f7dd6f5d64d2136d4d29
Vagner <vagner@gmail.com>
2019-06-19 18:17:43 -0300

    atualiza routes.js, PostController.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const PostController = require('./controllers/PostController');
 
 const routes = new express.Router();
 
-routes.get('/',(req, res) =>{
-  return res.send(`Olá ${req.query.name}`);
-});
+
+
+routes.post('/posts',PostController.store);
 
 module.exports = routes;
